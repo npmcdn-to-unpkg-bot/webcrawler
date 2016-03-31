a5c6db2260bca5b98a95a3a9b0e802a9ff532191
Desmond <xa_yhy@163.com>
2016-03-31 21:11:45 +0800

    week2_homework

--- homework_follow/week2/week2_homework/channel_extract.py
+++ homework_follow/week2/week2_homework/channel_extract.py
@@ -63,15 +63,23 @@ def get_item_info(item_pages):
 
 
 # channels = get_channels(start_url)
+# print(channels)
 #
 # pages = get_channel_pages(channels, 1)
 #
 # item_pages = get_item_pages(pages)
 # for i in item_pages:
 #     print(i)
-
 # get_item_info(item_pages)
-
-
 # get_item_info(['http://bj.ganji.com/jiaju/1802057617x.htm'])
+channels = ['http://bj.ganji.com/jiaju/', 'http://bj.ganji.com/rirongbaihuo/',
+            'http://bj.ganji.com/shouji/', 'http://bj.ganji.com/shoujihaoma/',
+            'http://bj.ganji.com/bangong/', 'http://bj.ganji.com/nongyongpin/',
+            'http://bj.ganji.com/jiadian/', 'http://bj.ganji.com/ershoubijibendiannao/',
+            'http://bj.ganji.com/ruanjiantushu/', 'http://bj.ganji.com/yingyouyunfu/',
+            'http://bj.ganji.com/diannao/', 'http://bj.ganji.com/xianzhilipin/',
+            'http://bj.ganji.com/fushixiaobaxuemao/', 'http://bj.ganji.com/meironghuazhuang/',
+            'http://bj.ganji.com/shuma/', 'http://bj.ganji.com/laonianyongpin/',
+            'http://bj.ganji.com/xuniwupin/', 'http://bj.ganji.com/qitawupin/',
+            'http://bj.ganji.com/ershoufree/', 'http://bj.ganji.com/wupinjiaohuan/']
 
