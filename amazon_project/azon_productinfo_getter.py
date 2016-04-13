aac019ffe84ced959163a0cab9e249b6fa235261
Desmond <xa_yhy@163.com>
2016-04-13 16:38:34 +0800

    azon productinfo getter added

---
+++ amazon_project/azon_productinfo_getter.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import pymongo
+
+azdb = pymongo.MongoClient('localhost', 27017)
+az = azdb['az']
+az_asin = az['az_asin']
+
+headers = {
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) \
+        AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+    }
+
+
+def get_page(p):
+    url = 'http://www.amazon.com/gp/aw/s/\
+    ref=is_pn_1?rh=i%3Aaps%2Ck%3Awrap+bracelet&page={}&keywords=pendant+necklace'.format(p)
+    return url
+
+
+def get_asin(url):
+    web_data = requests.get(url, headers=headers)
+    # print(web_data.text)
+    soup = BeautifulSoup(web_data.text, 'lxml')
+    asins = soup.select('#resultItems li > a')
+    # asin_list = []
+    for i in asins:
+        asin = i.get('data-asin')
+        if asin not in list(i['asin'] for i in az_asin.find()):
+            az_asin.insert_one({'asin': asin})
+            print(asin)
+        # asin_list.append(i.get('data-asin'))
+    # return asin_list
+
+
+if __name__ == '__main__':
+    for p in range(1, 10):
+        get_asin(get_page(p))
