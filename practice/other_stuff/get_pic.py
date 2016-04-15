274a1be93c7a9a897ed0f81451905b0b95172db1
Desmond <xa_yhy@163.com>
2016-04-15 15:13:52 +0800

    get_pic

---
+++ practice/other_stuff/get_pic.py
@@ -0,0 +1,6 @@
+import urllib.request
+import requests
+
+urllib.request.urlretrieve('http://bbs.static.oss.hoopchina.com.cn/\
+aHR0cDovL3MxLmR3c3RhdGljLmNvbS9ncm91cDEvTTAwL0EzLzMzLzAwYTg2MjJhZTlkZWYyNTMyNDc0ZGYzNTg4ODRhNmRjLmdpZg', 'pic.gif')
+requests.get('http://www.baidu.com')
